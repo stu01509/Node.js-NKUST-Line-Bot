3db0f0ebaeb5c06a01bfca5d9ad030ee4c1af49e
Cliff Su <stu01509@yahoo.com.tw>
2019-06-10 06:53:16 +0800

    feat: finished leave crawler and beautify.

--- libs/getData.js
+++ libs/getData.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 const LOGIN_URL = 'https://webap.nkust.edu.tw/nkust/perchk.jsp';
 const COURSE_URL = 'https://webap.nkust.edu.tw/nkust/ag_pro/ag222.jsp';
-
+const LEAVE_URL = 'https://webap.nkust.edu.tw/nkust/ak_pro/ak002_01.jsp';
 // enable cookie to pass next request
 request = request.defaults({ jar: true });
 
@@ -19,6 +19,12 @@ const courseInfo = {
   'arg02': '2',
 };
 
+const leaveInfo = {
+  'yms': '107,2',
+  'arg01': '107',
+  'arg02': '2',
+};
+
 const loginOptions = {
   url: LOGIN_URL,
   method: 'POST',
@@ -27,6 +33,12 @@ const loginOptions = {
 
 const courseOptions = {
   url: COURSE_URL,
+  method: 'POST',
+  form: courseInfo,
+};
+
+const leaveOptions = {
+  url: LEAVE_URL,
   method: 'POST',
   form: courseInfo,
 };
@@ -48,4 +60,23 @@ const getCourse = async () => {
   });
 };
 
-getCourse();
+// getCourse();
+
+const getLeave = () => {
+  request(loginOptions, (loginErr, loginRes) => {
+    if (loginErr || loginRes.statusCode !== 200) {
+      return;
+    }
+    request(leaveOptions, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        return;
+      }
+      const $ = cheerio.load(body);
+      $('body > table.LoginTable td').each((index, title) => {
+        console.log($(title).text());
+      });
+    });
+  });
+};
+
+getLeave();
