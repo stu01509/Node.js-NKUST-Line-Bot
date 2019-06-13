5254267b576c13addd6fa31a1e9105df7b836cea
Cliff Su <stu01509@yahoo.com.tw>
2019-06-13 18:13:42 +0800

    feat: finish connect to mongodb.

---
+++ libs/mongodb.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+
+require('dotenv').config();
+
+const { DB_PATH } = process.env;
+
+const connect = () => new Promise((resolve, reject) => {
+  const db = mongoose.connection;
+
+  db.on('error', () => {
+    console.log(`Can not connection to ${DB_PATH}`);
+    reject(process.exit(0));
+  });
+
+  db.on('open', () => {
+    console.log(`Connection to ${DB_PATH}`);
+    resolve();
+  });
+
+  db.on('disconnected', () => {
+    console.log('Connection disconnected');
+  });
+
+  mongoose.connect(DB_PATH, {
+    useNewUrlParser: true,
+  });
+});
+
+module.exports.connect = connect;
