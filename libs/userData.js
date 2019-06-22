4be450840223a3f9585a483a0843ac1fe7f63b92
Cliff Su <stu01509@yahoo.com.tw>
2019-06-22 20:56:57 +0800

    feat: finished user create and remove.

---
+++ libs/userData.js
@@ -0,0 +1,35 @@
+const UserSchema = require('../schema/user/User');
+
+const createMode = false;
+
+const userCreate = (userId, uid, pwd) => new Promise((resolve, reject) => {
+  const insertUserData = new UserSchema({
+    userId,
+    uid,
+    pwd,
+  });
+
+  insertUserData.save((err, result) => {
+    if (err) {
+      reject(new Error('登入失敗'));
+    } else {
+      resolve('登入成功');
+    }
+  });
+});
+
+const userRemove = userId => new Promise((resolve, reject) => {
+  UserSchema.deleteOne({
+    userId,
+  }, (err) => {
+    if (err) {
+      reject(new Error('刪除失敗'));
+    } else {
+      resolve('刪除成功');
+    }
+  });
+});
+
+module.exports.createMode = createMode;
+module.exports.userCreate = userCreate;
+module.exports.userRemove = userRemove;
